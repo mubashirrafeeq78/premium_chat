0df0f86f5a5916190730d16fed046917ad81dadf
mubashirrafeeq78 <officialmubashirhassan@gmail.com>
2026-04-09 07:38:34 +0500

    Remove unused API endpoint constants
    Removed unused API endpoint constants from config.

--- lib/config.dart
+++ lib/config.dart
@@ -7,9 +7,4 @@ class AppConfig {
 
   // آپ کی مخصوص سیکیورٹی کی
   static const String apiKey = "PixoChat_Master_Secure_2026";
-
-  // اینڈ پوائنٹس (بیک اینڈ کی فائلوں کے نام)
-  static const String auth = "/auth";
-  static const String verifyOtp = "/verify-otp";
-  static const String profile_setup = "/register-new-user";
 }
