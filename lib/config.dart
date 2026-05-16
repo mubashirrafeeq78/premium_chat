95b94171a496f36cd1aca122071b09ee3f71da9d
mubashirrafeeq78 <officialmubashirhassan@gmail.com>
2026-05-16 11:51:33 +0500

    Update baseUrl for server change

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   // 🔹 صرف یہی URL بدلیں گے جب بھی سرور چینج ہو
   static const String baseUrl =
-      "https://premiumchatbackend-production.up.railway.app";
+      "https://paxochat.com";
 
   // 🔹 API timeout (slow internet کیلئے مناسب)
   static const Duration timeout = Duration(seconds: 20);
